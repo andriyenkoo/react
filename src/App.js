78d9fa61307ff34540df75b930b7ba8cdabba109
Oleg Andriyenko <olegandrienko053@gmail.com>
2022-01-18 22:47:56 +0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    let [mission, setMission] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(flight => {
+                setMission(flight.filter(flight => flight.launch_year !== '2020'))
+            })
+    }, [])
+
+    return (
+        <div>
+            {
+                mission.map(value => <div key={value.flight_number}>
+                    {value.mission_name},
+                    {value.launch_year};
+                    <img src={value.links.mission_patch} alt=""/>
+                </div>)
+
+            }
+        </div>
+    );
+};
 
 export default App;
